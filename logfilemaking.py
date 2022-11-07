7671dd31d388b59070609c4976947ae9dd36a67d
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2022-11-07 17:28:52 +0530

    formatted the output

--- logfilemaking.py
+++ logfilemaking.py
@@ -12,20 +12,20 @@ with open('RandGameFilez.txt','a') as f:
     print("Game started at "+current_time,file=f)
 
 
-rand=random.randint(100,999)
+rand=3#random.randint(100,999)
 
 while try_again=="Y":
     n=int(input("Enter number: "))
     
     if n==rand:
-        current_time=str(datetime.datetime.now())
+        current_time=str(datetime.datetime.now().strftime("%H:%M:%S"))
         attempts+=1
         print("Congrats")
         with open('RandGameFilez.txt','a') as f:
-            print("attempt "+str(attempts)+", Your Guess was at "+current_time,file=f)
+            print("Correct guess. You cracked it on "+str(attempts)+"th attempt at "+current_time+"\n\n",file=f)
         try_again=""
     else:
-        current_time=str(datetime.datetime.now())
+        current_time=str(datetime.datetime.now().strftime("%H:%M:%S"))
         attempts+=1
         print("wrong try again")
         with open('RandGameFilez.txt','a') as f:
