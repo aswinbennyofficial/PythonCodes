51c9c7037aa72525fc775d5811600f073d3d792c
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2022-11-07 17:41:49 +0530

    final update

--- logfilemaking.py
+++ logfilemaking.py
@@ -1,33 +1,37 @@
 import random
-import datetime
+import datetime 
 
  
 try_again="Y"
 attempts=0
 
 
-current_time=str(datetime.datetime.now().strftime("%H:%M:%S"))
+current_time=str(datetime.datetime.now().strftime("%H:%M:%S")) #fetches current time
 print("Current time is: ",current_time)
 with open('RandGameFilez.txt','a') as f:
-    print("Game started at "+current_time,file=f)
+    print("Game started at "+current_time,file=f) #logs it to the file
 
 
-rand=3#random.randint(100,999)
+rand=random.randint(100,999) #generates random number
+#print(rand)
 
 while try_again=="Y":
-    n=int(input("Enter number: "))
-    
-    if n==rand:
-        current_time=str(datetime.datetime.now().strftime("%H:%M:%S"))
-        attempts+=1
-        print("Congrats")
-        with open('RandGameFilez.txt','a') as f:
-            print("Correct guess. You cracked it on "+str(attempts)+"th attempt at "+current_time+"\n\n",file=f)
-        try_again=""
+    n=int(input("Guess the number: "))
+    if 100<=n<=999:
+        if n==rand:
+            current_time=str(datetime.datetime.now().strftime("%H:%M:%S"))
+            attempts+=1
+            print("Congrats. Thanks for playing")
+            with open('RandGameFilez.txt','a') as f:
+                print("It took you "+str(attempts)+" attempts to guess the code at time "+current_time+"\n\n",file=f)
+            try_again=""
+        else:
+            current_time=str(datetime.datetime.now().strftime("%H:%M:%S"))
+            attempts+=1
+            print("wrong try again")
+            with open('RandGameFilez.txt','a') as f:
+                print("attempt "+str(attempts)+", Your Guess was at "+current_time,file=f)
     else:
-        current_time=str(datetime.datetime.now().strftime("%H:%M:%S"))
-        attempts+=1
-        print("wrong try again")
-        with open('RandGameFilez.txt','a') as f:
-            print("attempt "+str(attempts)+", Your Guess was at "+current_time,file=f)
+        print("Number is out of range")
+
         
